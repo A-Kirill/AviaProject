12550a1f15032779a36439035a5bc4b60f2ece63
Kirill Anisimov <anisimov_k@icloud.com>
2020-06-14 15:35:03 +0300

    added MapViewController and func cityForLocation  in DataManager

---
+++ AviaProject/MapViewController.h
@@ -0,0 +1,19 @@
+//
+//  MapViewController.h
+//  AviaProject
+//
+//  Created by Kirill Anisimov on 14.06.2020.
+//  Copyright © 2020 Кирилл Анисимов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "DataManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MapViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AviaProject/MapViewController.m
@@ -0,0 +1,97 @@
+//
+//  MapViewController.m
+//  AviaProject
+//
+//  Created by Kirill Anisimov on 14.06.2020.
+//  Copyright © 2020 Кирилл Анисимов. All rights reserved.
+//
+
+#import "MapViewController.h"
+#import "LocationService.h"
+#import "APIManager.h"
+#import "MapPrice.h"
+
+
+@interface MapViewController () <MKMapViewDelegate>
+
+@property (strong, nonatomic) MKMapView *mapView;
+@property (nonatomic, strong) LocationService *locationService;
+@property (nonatomic, strong) City *origin;
+@property (nonatomic, strong) NSArray *prices;
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    
+    CGRect frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    
+  //  self.title = @"Карта цен";
+    
+    _mapView = [[MKMapView alloc] initWithFrame:frame];
+    _mapView.showsUserLocation = true;
+    [self.view addSubview:_mapView];
+    [self.mapView setDelegate:self];
+    
+    [[DataManager sharedInstance] loadData];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dataLoadedSuccessfully) name:kDataManagerLoadDataDidComplete object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateCurrentLocation:) name:kLocationServiceDidUpdateCurrentLocation object:nil];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)dataLoadedSuccessfully {
+    _locationService = [[LocationService alloc] init];
+}
+
+- (void)updateCurrentLocation:(NSNotification *)notification {
+    CLLocation *currentLocation = notification.object;
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(currentLocation.coordinate, 1000000, 1000000);
+    [_mapView setRegion: region /*animated: YES*/];
+    
+    if (currentLocation) {
+        _origin = [[DataManager sharedInstance] cityForLocation:currentLocation];
+        if (_origin) {
+            [[APIManager sharedInstance] mapPricesFor:_origin withCompletion:^(NSArray *prices) {
+                [self setPrices: prices];
+            }];
+        }
+    }
+}
+
+- (void)setPrices:(NSArray *)prices {
+    _prices = prices;
+    [_mapView removeAnnotations: _mapView.annotations];
+    
+    for (MapPrice *price in prices) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+            annotation.title = [NSString stringWithFormat:@"%@ (%@)", price.destination.name, price.destination.code];
+            annotation.subtitle = [NSString stringWithFormat:@"%ld руб.", (long)price.value];
+            annotation.coordinate = price.destination.coordinate;
+            [self.mapView addAnnotation: annotation];
+        });
+    }
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation {
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+    
+    MKAnnotationView *annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"loc"];
+    annotationView.canShowCallout = YES;
+    annotationView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    
+    return annotationView;
+}
+
+
+@end
